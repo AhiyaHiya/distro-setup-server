1bab3fe2efa417002d3508015f56df6a4fdb1f37
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-11-14 11:07:19 -0700

    Initial commit of system install scripts for pipx yt-dlp and ffmpeg

---
+++ install/system.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for installer in ./system/*.sh; do source $installer; done
---
+++ install/system/yt-dlp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pipx install yt-dlp==2025.10.14 --dry-run
+pipx install yt-dlp==2025.10.14
+yt-dlp --version
