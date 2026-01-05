3accd1f022b0c4208765040ccace0c9672d464ed
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2026-01-05 21:44:53 +0000

    Add missing commands from webmin script

--- install/system/webmin.sh
+++ install/system/webmin.sh
@@ -3,3 +3,7 @@
 curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
 sudo sh webmin-setup-repo.sh
 
+sudo apt update
+sudo apt install --install-recommends webmin
+sudo systemctl status webmin
+
