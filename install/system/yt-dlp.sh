0d661c33a1455faeb9ecf4ee30a3add3f873b0d5
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2026-02-12 17:17:20 +0000

    Add DENO install for yt-dlp script

--- install/system/yt-dlp.sh
+++ install/system/yt-dlp.sh
@@ -3,3 +3,7 @@
 pipx install yt-dlp==2025.10.14 --dry-run
 pipx install yt-dlp==2025.10.14
 yt-dlp --version
+
+# For challenge questions, DENO has to be installed too
+curl -fsSL https://deno.land/install.sh | sh
+
